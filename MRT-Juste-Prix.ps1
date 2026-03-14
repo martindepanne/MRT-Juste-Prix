a11b48c71d2ed0d9dd698e56db0735c257b41e74
Martin-Dépanne <martindepanne@users.noreply.github.com>
2026-03-14 23:56:11 +0100

    Create MRT-Juste-Prix.ps1

---
+++ MRT-Juste-Prix.ps1
@@ -0,0 +1,13 @@
+﻿[int]$randomNumber = Get-Random -Minimum 1 -Maximum 100
+
+[int]$choisis = Read-Host "Choisi un nombre entre 1 et 100"
+ 
+while ($choisis -ne $randomNumber) {
+if ($choisis -lt $randomNumber) 
+{ Write-Host "Plus grand" }
+else
+{ Write-Host "Plus Petit" }
+ [int]$choisis = Read-Host "Choisi un nombre entre 1 et 100"
+}
+
+Write-Host "Bien joué"
